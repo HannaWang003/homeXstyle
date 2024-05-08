870abbfe83b30d85b005cc3e245bab27a0fc0ebc
HannaWang003 <hannawang003@gmail.com>
2024-05-08 13:42:39 +0800

    init

---
+++ api/db.php
@@ -0,0 +1,59 @@
+<?php
+date_default_timezone_set('Asia/Taipei');
+session_start();
+//2
+function dd($ary)
+{
+    echo "<pre>";
+    print_r($ary);
+    echo "</pre>";
+}
+function to($url)
+{
+    header("location:$url");
+}
+//
+class DB
+{
+    //3
+    protected $table;
+    protected $pdo;
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=program-homeXstyle";
+    //construct
+    function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+    //3
+    private function a2s($ary)
+    {
+        foreach ($ary as $col => $val) {
+            $tmp[] = "`$col`='$val'";
+        }
+        return $tmp;
+    }
+    private function sql_all($sql, $where, $other)
+    {
+        if (is_array($where)) {
+            $sql .= " where " . join(" && ", $this->a2s($where));
+        } else {
+            $sql .= " $where";
+        }
+        $sql .= " $other";
+        return $sql;
+    }
+    private function math($math, $col, $where = "", $other = "")
+    {
+        $sql = "select $math(`$col`) from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function all($where = '', $other = '')
+    {
+        $sql = "select * from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+$Carousel = new DB('carousel');
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+include_once "./api/db.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home X style</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+        integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+        crossorigin='anonymous' />
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js'
+        integrity='sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=='
+        crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js'
+        integrity='sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ=='
+        crossorigin='anonymous'></script>
+</head>
+
+<body>
+    <!-- nav -->
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="./img/logo.png" alt="Bootstrap" width="50" height="50">
+            </a>
+        </div>
+    </nav>
+    <!-- /nav-->
+    <!-- carousel -->
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <?php
+            $carousels = $Carousel->all();
+            foreach ($carousels as $carousel) {
+            ?>
+            <div class="carousel-item active">
+                <img src="./img/plant/<?= $carousel['img'] ?>" class="d-block w-100" alt="...">
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- /carousel -->
+
+
+</body>
+
+</html>
